5d931944e922c5415041754ff10f7d156ea00d46
Ahtisham Afzal <aahtisham925@gmail.com>
2022-12-03 23:09:34 +0500

    Add project files.

---
+++ MobileSellingApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MobileSellingApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
